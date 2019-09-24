2c17437c1db4385e139d10bad8f6d4e5839f455c
Luillin Escobar <contacto@contratistaweb.com>
2019-09-24 03:42:54 -0500

    desarollo funcion que intercala los colores del titulo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
-$("h1").animate({
-   color: "#f86"
-}, 3000); 
+$(function(){
+	function Color1(titulo){
+        $( ".main-titulo" ).animate({
+          color: "#DCFF0E",
+        }, 1000,function(){
+					Color2(titulo);
+				}
+	);}
+		function Color2(titulo){
+        $( ".main-titulo" ).animate({
+          color: "#FFF",
+        }, 1000,function(){
+					Color1(titulo);
+				}
+	);}
+Color1();
+        
+      
 
+	
+});	
