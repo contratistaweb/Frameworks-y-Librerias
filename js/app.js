e272442912aef2934d687b325596a6a116799ede
Luillin Escobar <contacto@contratistaweb.com>
2019-10-02 01:55:12 -0500

    funcion timer agregada

--- js/app.js
+++ js/app.js
@@ -1,21 +1,166 @@
 $(function(){
-	function Color1(titulo){
-        $( ".main-titulo" ).animate({
-          color: "#DCFF0E",
-        }, 1000,function(){
-					Color2(titulo);
-				}
-	);}
-		function Color2(titulo){
-        $( ".main-titulo" ).animate({
-          color: "#FFF",
-        }, 1000,function(){
-					Color1(titulo);
-				}
-	);}
-Color1();
-        
+  function Color1(titulo){
+    $('.main-titulo').animate({
+      color: "#DCFF0E",
+    }, 1000,function(){
+      Color2(titulo);
+      Mover();
       
+    });
+  }
+	function Color2(titulo){
+		$('.main-titulo').animate({
+			color: "#FFF",
+		}, 1000,function(){
+			Color1(titulo);
+    });
+  }
+  Color1();
+  //<---   fin efecto titulo   --->
 
-	
-});	
+  //<---   Crear objetos - Dulces
+  function crear(){
+    var i = 1;
+    while(i<=7){
+      if($('.col-'+i+' img').length == 0 || $('.col-'+i+' img').length<=6 && $('img').length <= 48){
+        $('.col-'+i).prepend('<img src="image/'+aleatorio(1, 5)+'.png" id=""></img>');
+        
+        continue;
+      }
+      imgid();
+      estilos();
+      i++;
+    }
+  }
+
+
+  //<---> funcion Asignacion de id
+  function imgid(){
+    var $idel = $('.col-1 img');
+    for(x=0;x<$idel.length;x++){
+      if($($('.col-1 img')[x]).attr('id')==''){
+        $($('.col-1 img')[x]).attr('id','img-1'+x);
+      }else{
+          if($($('.col-2 img')[x]).attr('id')==''){
+            $($('.col-2 img')[x]).attr('id','img-2'+x);
+          }else{
+            if($($('.col-3 img')[x]).attr('id')==''){
+              $($('.col-3 img')[x]).attr('id','img-3'+x);
+            }else{
+              if($($('.col-4 img')[x]).attr('id')==''){
+                $($('.col-4 img')[x]).attr('id','img-4'+x);
+              }else{
+                if($($('.col-5 img')[x]).attr('id')==''){
+                  $($('.col-5 img')[x]).attr('id','img-5'+x);
+                }else{
+                  if($($('.col-6 img')[x]).attr('id')==''){
+                    $($('.col-6 img')[x]).attr('id','img-6'+x);
+                  }else{
+                    if($($('.col-7 img')[x]).attr('id')==''){
+                      $($('.col-7 img')[x]).attr('id','img-7'+x);
+                    };
+                  }
+                }
+              }
+            }
+          }
+      }
+      $('.col-'+[x+1]).attr('id','sortable'+[x+1]).addClass('connectedSortable');
+      $('img').addClass('ui-state-default');
+    }
+  };
+
+  //<---   Numeros aleatorios
+  function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  //<---> funcion Limpiar
+  function limpiar(){
+    $( ".panel-tablero div" ).empty();
+  }
+
+  //<--->  boton inicio reinicio
+  $('.btn-reinicio').click(function (e) {
+    e.preventDefault();
+    if($(this).text() == 'Iniciar' ){
+      $(this).text('Reiniciar');
+      Timer()
+      estilos();
+      crear();
+      //arrastrable();
+    }else{
+      if($(this).text() == 'Reiniciar' ){
+        $(this).text('Iniciar');
+        estilos();
+        clearInterval(timer);
+        limpiar();
+      }    
+    }
+  });
+
+  //funcion contador de clicks --> no se visualiza a un en el panel, falta por asociar ;-)
+  
+
+
+ 
+
+
+function estilos(){
+  $('img').css({'height': '96px'});
+}
+
+  // <--- draggable ---> condicionar para que se eliminen las necesarias
+  function Mover() {
+    $( "#img-10" ).draggable();
+    $( "#img-20" ).droppable({
+      accept: "#img-10",
+      classes: {
+        "ui-droppable-active": "ui-state-active",
+        "ui-droppable-hover": "ui-state-hover"
+      },
+      drop: function( event, ui ) {
+        $("#img-20").attr('src',$("#img-10").attr('src'));
+        $("#img-10").remove();
+        crear();
+        
+      }
+    });
+  }
+
+  function movimientos(){
+  var i = 0;
+  $('img').click(function(){ 
+    if (i < 200 ) { /*Cambiar el >= 0 por < 10 si quieres limitar el incremento*/
+      i++;
+      } else if (i = i++) {
+      i = 0;
+      }
+    $('#movimientos-text').data(i);
+  }); movimientos();
+
+
+  // $('#disminuye').click(function(){ 
+
+  //  if (i > 0) {--i;} 
+  //        document.getElementById("display").innerHTML = i;
+  // })
+  }
+
+  //<---   fin funcion principal   --->
+}());
+
+//evento en al drop
+//$('#img-14').detach()-->
+
+//<---> efecto para desaparecer las imagenes al juntar
+//$('img').hide('scale',{percent: 200, direction: 'vertical' },1000);
+
+//<---> Para temporizador
+//var timeoutID = scope.setTimeout(function[, delay, param1, param2, ...]);
+//var timeoutID = scope.setTimeout(function[, delay]);
+//var timeoutID = scope.setTimeout(code[, delay]); 
+//function explode(){ alert("Boom!"); } setTimeout(explode, 2000); 
+
+// para cuadro elemento desplegable...
+//$( ".elemento" ).toggle('scale')
--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,35 @@
-function mensaje() {
-	console.log("hola desde javascript");
+function Timer(){
+var limite = new Date();
+var end = limite.getTime()+120000;
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var ahora = new Date();
+        var now = ahora.getTime();
+        var distance = end - now;
+        if (distance < 0 || $('.btn-reinicio').text() == 'Iniciar') {
+
+			clearInterval(timer);
+            document.getElementById('timer').innerHTML = '02:00';
+
+            return;
+        }
+
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+		document.getElementById('timer').innerHTML = '';
+        document.getElementById('timer').innerHTML += hours;
+        document.getElementById('timer').innerHTML += minutes + ':';
+        document.getElementById('timer').innerHTML += seconds;
+    }
+
+	timer = setInterval(showRemaining, 1000);
 }
-setTimeout(mensaje,5000);
