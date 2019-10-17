b5843133c9f39ad8684897b07233164c1e8cfcfc
Luillin Escobar <contacto@contratistaweb.com>
2019-10-16 22:00:13 -0500

    valido grupode tres o mas seguidas con siguiente movimiento

--- js/app.js
+++ js/app.js
@@ -1,54 +1,55 @@
-$(function(){
+$(function () {
   //efecto titulo
-  function Color1(titulo){
+  function Color1(titulo) {
     $('.main-titulo').animate({
       color: "#DCFF0E",
-    }, 1000,function(){
+    }, 1000, function () {
       Color2(titulo);
-      
+
     });
-    
+
   }
-	function Color2(titulo){
-		$('.main-titulo').animate({
-			color: "#FFF",
-		}, 1000,function(){
-			Color1(titulo);
+  function Color2(titulo) {
+    $('.main-titulo').animate({
+      color: "#FFF",
+    }, 1000, function () {
+      Color1(titulo);
     });
   }
   Color1();
   //<---> funcion Limpiar
-  function limpiar(){
-    $( ".panel-tablero div" ).empty();
+  function limpiar() {
+    $(".panel-tablero div").empty();
     $("#movimientos-text").text('0');
     $("#score-text").text('0');
   }
   //<--->  boton inicio reinicio
   $('.btn-reinicio').click(function (e) {
     e.preventDefault();
-    if($(this).text() == 'Iniciar' ){
+    if ($(this).text() == 'Iniciar') {
       $(this).text('Reiniciar');
       limpiar();
       Timer();
       crear();
-      $( '.panel-tablero' ).show("drop", { direction: "left" }, "slow" , function(){
-        if($('.panel-tablero').attr('style') == ''){
+      $('.panel-tablero').show("drop", { direction: "left" }, "slow", function () {
+        if ($('.panel-tablero').attr('style') == '') {
           $('.panel-score').animate({
             transform: 'width',
-            width:'-=75%'
-          },1000);
+            width: '-=75%'
+          }, 1000);
         }
       });
-    }else{
-      if($(this).text() == 'Reiniciar' ){
+      eliminar() /* agregado este llamado */
+    } else {
+      if ($(this).text() == 'Reiniciar') {
         $(this).text('Iniciar');
         estilos();
-        $( '.panel-tablero' ).hide("drop", { direction: "left" }, "slow" , function(){
-          if($('.panel-tablero').attr('style') !== ''){
+        $('.panel-tablero').hide("drop", { direction: "left" }, "slow", function () {
+          if ($('.panel-tablero').attr('style') !== '') {
             $('.panel-score').animate({
               transform: 'width',
-              width:'+=43%'
-            },1000);
+              width: '+=43%'
+            }, 1000);
           }
         });
       }
@@ -57,41 +58,41 @@ $(function(){
 
   //< ========= drag and drop  Next U ========>
   function crear() {
-    if($('.btn-reinicio').text() != 'Iniciar' ){
-    var images = [
-      "image/1.png",
-      "image/2.png",
-      "image/3.png",
-      "image/4.png",
-      "image/1.png",
-      "image/2.png",
-      "image/3.png"
-    ]
-    var columnas = $(".panel-tablero div");
-  
-    for (var i = 0; i <= columnas.length; i++) {
-      for (var j = 0; j < images.length; j++) {
-        var img = Math.floor(Math.random() * (4 - 1 + 1));
-        if($('[class="col-'+[i]+'"] img').length < 7){
-          $('[class="col-'+[i]+'"]').prepend("<img src='" + images[img] +
-          "' class='elemento' width='100px' height='100px' id='"+[i+1]+[j+1]+"'>");
-          
-          draganddrop();
-          
+    if ($('.btn-reinicio').text() != 'Iniciar') {
+      var images = [
+        "image/1.png",
+        "image/2.png",
+        "image/3.png",
+        "image/4.png",
+        "image/1.png",
+        "image/2.png",
+        "image/3.png"
+      ]
+      var columnas = $(".panel-tablero div");
+
+      for (var i = 0; i <= columnas.length; i++) {
+        for (var j = 0; j < images.length; j++) {
+          var img = Math.floor(Math.random() * (4 - 1 + 1));
+          if ($('[class="col-' + [i] + '"] img').length < 7) {
+            $('[class="col-' + [i] + '"]').prepend("<img src='" + images[img] +
+              "' class='elemento' width='100px' height='100px' id='" + [i + 1] + [j + 1] + "'>");
+
+            draganddrop();
+
+          }
         }
       }
     }
   }
-}
-  
-  function draganddrop(){
+
+  function draganddrop() {
     var columnas = $(".panel-tablero div");
     for (var i = 0; i < columnas.length; i++) {
       elementos = $(columnas[i]).find("img");
-  
+
       for (var j = 0; j < elementos.length; j++) {
         var start_left, stop_left, start_top, stop_top;
-  
+
         $(elementos[j]).draggable({
           disabled: false,
           revert: "invalid",
@@ -114,33 +115,33 @@ $(function(){
           },
           accept: function (ui) {
             if ((start_left < stop_left) || (start_left > stop_left) || (start_top < stop_top) || (
-                start_top > stop_top)) {
+              start_top > stop_top)) {
               return true;
             }
             return false;
           },
           drop: function (event, ui) {
-  
+
             $($(ui.draggable)).css({
               left: "auto",
               top: "auto"
             });
-  
+
             validarMovimiento($(this), $(ui.draggable), start_left, stop_left, start_top, stop_top);
-  
+
             var movimientos = parseInt($("#movimientos-text").text());
             $("#movimientos-text").text(movimientos + 1);
-            $("#score-text").text((movimientos + 1)*100);
-            
+            $("#score-text").text((movimientos + 1) * 100);
+
             eliminar();
           }
         });
       }
     }
   }
-  
+
   function validarMovimiento(dropE, dragE, start_left, stop_left, start_top, stop_top) {
-  
+
     if (start_top > stop_top) {
       cambiar($(dropE), $(dragE));
     } else if (start_top < stop_top) {
@@ -150,9 +151,9 @@ $(function(){
     } else if (start_left > stop_left) {
       cambiar($(dropE), $(dragE));
     }
-  
+
   }
-  
+
   function cambiar(dropE, dragE) {
     if ($(dropE).next().length != 0) {
       if ($(dragE).next().length != 0) {
@@ -172,62 +173,45 @@ $(function(){
     }
   }
   //<=========== fin Drag and Drop =========>
-  
+
   function eliminar() {
     for (var i = 1; i <= 7; i++) {
       for (var z = 0; z <= 6; z++) {
-        var dulce1 = $($('.col-' + [i]).find("img")[z]);
-        var dulce2 = $($('.col-' + [i]).find("img")[z + 1]);
-        var dulce3 = $($('.col-' + [i]).find("img")[z + 2]);
-        var dulce4 = $($('.col-' + [i + 1]).find("img")[z]);
-        var dulce5 = $($('.col-' + [i + 2]).find("img")[z]);
-        if (dulce1.attr('src') == dulce2.attr('src')
-          && dulce2.attr('src') == dulce3.attr('src')) {
-          dulce1.effect('pulsate',function(){
-            $(this).remove();
-            crear();
-          });
-          dulce2.effect('pulsate',function(){
-            $(this).remove();
-            crear();
-          });
-          dulce3.effect('pulsate',function(){
-            $(this).remove();
-            crear();
-          });
-          
-          console.log(dulce1.attr('src'), dulce2.attr('src'),dulce3.attr('src'));
-          
-          
-        }else if(dulce1.attr('src') == dulce4.attr('src')
-          && dulce4.attr('src') == dulce5.attr('src')){
-            dulce1.effect('pulsate',function(){
-              $(this).remove();
-              crear();
-            });
-            dulce4.effect('pulsate',function(){
-              $(this).remove();
-              crear();
-            });
-            dulce5.effect('pulsate',function(){
-              $(this).remove();
-              crear();
-            });
-            
-              console.log(dulce1.attr('src'), dulce4.attr('src'),dulce5.attr('src'));
-            
-          
+        /* Valida Columnas */
+        var dulcec1 = $($('.col-' + [i]).find("img")[z]);
+        var dulcec2 = $($('.col-' + [i]).find("img")[z + 1]);
+        var dulcec3 = $($('.col-' + [i]).find("img")[z + 2]);
+        if (dulcec1.attr('src') == dulcec2.attr('src')
+          && dulcec2.attr('src') == dulcec3.attr('src')) {
+          dulcec1.addClass("eliminar");
+          dulcec2.addClass("eliminar");
+          dulcec3.addClass("eliminar");
         }
-    
+        /* Valida Filas */
+        var dulcef1 = $($('.col-' + [i]).find("img")[z]);
+        var dulcef2 = $($('.col-' + [i + 1]).find("img")[z]);
+        var dulcef3 = $($('.col-' + [i + 2]).find("img")[z]);
+        if (dulcef1.attr('src') == dulcef2.attr('src')
+          && dulcef2.attr('src') == dulcef3.attr('src')) {
+          dulcef1.addClass("eliminar");
+          dulcef2.addClass("eliminar");
+          dulcef3.addClass("eliminar");
+        }
       }
-    
+
     }
-    
+/* Elimina todos los dulces */
+    $(".eliminar").effect('pulsate', function () {
+      $(this).remove();
+      crear();
+    });
+
   }
 
-
-  function estilos(){
-    $('img').css({'height': '96px'});
+/*
+  function estilos() {
+    $('img').css({ 'height': '96px' });
   }
+*/
   //<---   fin funcion principal   --->
 }());
